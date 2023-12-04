9938c378ebf7f486a4bf802d22c71d8cb01b3d41
JsonSept <jasonseptember.za@gmail.com>
2023-12-04 23:05:14 +0200

    updated my folders structure

--- js/checkout.js
+++ js/checkout.js
@@ -1,6 +1,3 @@
 let h1 = document.querySelector('h1')
- home
 h1.innerText = "Checkout"
 
-h1.innerText = "Checkout"
- main
--- js/contact.js
+++ js/contact.js
@@ -1,2 +1,2 @@
-let h1 = document.querySelector('h1')
+let h1 = document.querySelector('h1');
 h1.innerText = "Contact"
